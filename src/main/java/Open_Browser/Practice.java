6c7f27072cad1dc15b131ecc3725208e41b2a878
kondajisaikumar <kondajisaikumar.marolix@gmail.com>
2023-10-27 13:53:27 +0530

    new class added

--- src/main/java/Open_Browser/Practice.java
+++ src/main/java/Open_Browser/Practice.java
@@ -11,11 +11,11 @@ import org.openqa.selenium.interactions.Actions;
 public class Practice {
 
 	public static void main(String[] args) throws InterruptedException {
-		WebDriver driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.get("https://www.myntra.com/?utm_source=dms_google&utm_medium=searchbrand_cpc&utm_campaign=dms_google_searchbrand_cpc_Search_Brand_Myntra_Brand_India_BM_TROAS_SOK_New&gclid=Cj0KCQjwmvSoBhDOARIsAK6aV7ier9xBTFWxfEDuoURjR3STdgFHC4Chc0fuErnAKC24fG_i3BEEGhoaAk_iEALw_wcB");
-      WebElement clk= driver.findElement(By.xpath("//a[text()='Home & Living']"));
-      
+//		WebDriver driver = new ChromeDriver();
+//		driver.manage().window().maximize();
+//		driver.get("https://www.myntra.com/?utm_source=dms_google&utm_medium=searchbrand_cpc&utm_campaign=dms_google_searchbrand_cpc_Search_Brand_Myntra_Brand_India_BM_TROAS_SOK_New&gclid=Cj0KCQjwmvSoBhDOARIsAK6aV7ier9xBTFWxfEDuoURjR3STdgFHC4Chc0fuErnAKC24fG_i3BEEGhoaAk_iEALw_wcB");
+//      WebElement clk= driver.findElement(By.xpath("//a[text()='Home & Living']"));
+//      
       
     Actions act=new Actions(driver);
     Thread.sleep(2000);
